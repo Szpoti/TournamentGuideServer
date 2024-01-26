6c1c782f55b51a74fa7b0bdead262d8d671a5bcc
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-01-26 15:19:41 +0100

    Add admin login option

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentGuideServer.Models;
+
+namespace TournamentGuideServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController
+    {
+        private readonly string? _apiKey;
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _apiKey = configuration["API_KEY"];
+        }
+
+        [HttpGet("login", Name = "AdminLogin")]
+        public bool Login(string apiKey)
+        {
+            return apiKey == _apiKey;
+        }
+    }
+}
